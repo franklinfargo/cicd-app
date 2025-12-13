690a737901e0f70e9d9d3464a42b725b5934b1e1
franklinfargo <franklin.f@landmarkindia.net>
2025-12-13 19:33:15 +0530

    19:33

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
-Route::get('/', function () {
+// Route::get('/', function () {
     
-    return view('welcome');
-});
+//     return view('welcome');
+// });
 
-Route::get('/users', [UserController::class, 'index']);
+// Route::get('/users', [UserController::class, 'index']);
+
+
+Route::get('/', [UserController::class, 'index']);
